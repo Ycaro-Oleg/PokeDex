530bd7754edcdbbc788eeaf1f900b4ea46c9d6bd
Ycaro-Oleg <ycaro.oleg@gmail.com>
2023-09-20 15:19:59 -0300

    feature/Added colors according to type and a button to load and limit by 5 the pokemons on screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,35 @@
-function convertPokemonTypesToLi(pokemonTypes){
-    return pokemonTypes.map((typeSlot) =>`<li class="type">${typeSlot.type.name}</li>`)
-}
-
-
-
-
-function convertPokemonToLi(pokemon){
-    return `
-    <li class="pokemon">
-                <span class="number">#${pokemon.order}</span>
-                <span class="name">${pokemon.name}</span>
-            <div class="detail">
-                <ol class="types">
-                    ${convertPokemonTypesToLi(pokemon.types).join('')}
-                </ol>
-
-                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
-            </div>
-            </li>
-    `
-}
 
 const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5
+let offset = 0 
 
-pokeApi.getPokemons().then((pokemons =[]) => {
-        pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+
+
+function loadPokemonItens(offset,limit){
+
+    pokeApi.getPokemons(offset,limit).then((pokemons =[]) => {
+       const newHtml = pokemons.map((pokemon) => `
+        <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type)=> `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+    
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+                </li>
+        `
+    ).join('')
+        pokemonList.innerHTML += newHtml
     })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click',() =>{
+    offset += limit
+    loadPokemonItens(offset,limit)
+})
