b54297dea24c01f3244c381946e423a12b9f4916
Ycaro-Oleg <ycaro.oleg@gmail.com>
2023-09-20 10:33:45 -0300

    feature/Simplified the main.js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,3 @@
-const offset= 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 function convertPokemonToLi(pokemon){
     return `
     <li class="pokemon">
@@ -21,13 +17,6 @@ function convertPokemonToLi(pokemon){
 
 const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons) => {
-        const listItems = []
-
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];      
-            listItems.push(convertPokemonToLi(pokemon)) 
-        }
-
-        console.log(listItems)
+pokeApi.getPokemons().then((pokemons =[]) => {
+        pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
     })
